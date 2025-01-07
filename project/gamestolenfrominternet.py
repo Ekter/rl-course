d2e262aa78ee11ac31681c2c35254394e1ebca87
Matthias Vermot - Desroches <115169567+MatthiasVermot-Desroches@users.noreply.github.com>
2025-01-07 16:52:50 +0100

    cadoizo

--- project/gamestolenfrominternet.py
+++ project/gamestolenfrominternet.py
@@ -4,15 +4,30 @@ from ursina import *
 import random as r
 
 app = Ursina()
+
 window.fullscreen = True
 window.color = color.white
 
+dino = Animation("assets/dino", x=-5)
 
-dino = Animation("assets/dino", collider="box", x=-5)
+head_collider = Entity(
+    model='sphere',
+    collider='sphere',
+    parent=dino,
+    scale=(1, 1, 1),  # Taille pour la tête
+    position=(0, 0, 0),
+    enabled=True  # Actif par défaut
+)
 
 ground1 = Entity(model="quad", texture="assets/ground", scale=(50, 0.25, 1), z=1, y=-0.5)
 ground2 = duplicate(ground1, x=50)
 pair = [ground1, ground2]
+
+cadoizo = Entity(model="quad",texture = "assets/cadoizo", x = 200, y = 0.1, collider = "box")
+cadoizo2 = Entity(model="quad",texture = "assets/cadoizo", x = 10, y = 1, collider = "box")
+cadoizos = [cadoizo,cadoizo2]
+
+
 
 
 gordo1 = Animation(name="assets/gordo", x=10, collider="box")
@@ -20,6 +35,14 @@ gordo2 = Animation(name="assets/gordo", x=14, collider="box")
 gordo3 = Animation(name="assets/gordo", x=20, collider="box")
 gordo4 = Animation(name="assets/gordo", x=26, collider="box")
 gordos = [gordo1, gordo2, gordo3, gordo4]
+
+collider_visual = Entity(
+    model='wireframe_quad',  # Modèle filaire pour visualiser
+      # Correspond à la taille du collider
+    position=dino.position,  # Même position que l'entité
+    color=color.red,  # Rouge pour bien le distinguer
+    parent=dino # Parenté avec l'entité principale pour qu'il bouge avec
+)
 
 
 def newGordo():
@@ -48,6 +71,8 @@ def update():
             ground.x += 100
     for c in gordos:
         c.x -= 6 * time.dt
+    for d in cadoizos:
+        d.x -=6 *time.dt
     if dino.intersects().hit:
         dino.texture = "assets/hit"
         application.pause()
